ea1e64db9f1089712fe8c65ed3ac356f7026d182
Lucas Apaza <apazalucas69@gmail.com>
2024-01-31 12:39:02 -0300

    Inicio de sesion

---
+++ login/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sistemas de ventas</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../public/template/AdminLTE-3.2.0/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="../public/template/AdminLTE-3.2.0/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../public/template/AdminLTE-3.2.0/dist/css/adminlte.min.css">
+
+  <!-- sweetalert2 -->
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+
+<body class="hold-transition login-page">
+  <div class="login-box">
+    <!-- /.login-logo -->
+
+    <?php
+    session_start();
+    if (isset($_SESSION['mensaje'])) {
+      $respuestas = $_SESSION['mensaje']; ?>
+
+      <script>
+        Swal.fire({
+          icon: "error",
+          title: "Fatal Error",
+          text: "<?php echo $respuestas?>",
+        });
+      </script>
+
+    <?php
+    unset($_SESSION['mensaje']);
+    }
+
+    ?>
+    <center><img src="https://img.freepik.com/vector-gratis/analistas-demanda-dandose-mano-pantallas-portatiles-planificando-demanda-futura-planificacion-demanda-analisis-demanda-ilustracion-concepto-pronostico-ventas-digitales_335657-2098.jpg?w=740&t=st=1706050927~exp=1706051527~hmac=ff643f580a16acae985b2b6496bc0d12f11f9a46471e0fad10a7cb7f0efb5c3a" alt="imagen" width="300px"></center>
+    <br>
+    <div class="card card-outline card-primary">
+      <div class="card-header text-center">
+        <a href="../public/template/AdminLTE-3.2.0/index2.html" class="h1"><b>Sistemas de </b>VENTAS</a>
+      </div>
+      <div class="card-body">
+        <p class="login-box-msg">Ingrese sus datos</p>
+
+        <form action="../app/controllers/login/ingreso.php" method="post">
+          <div class="input-group mb-3">
+            <input type="email" name="email" class="form-control" placeholder="Email">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="password" name="password_user" class="form-control" placeholder="Password">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
+            </div>
+          </div>
+          <hr>
+          <div class="row">
+            <!-- /.col -->
+            <div class="col-12">
+              <button type="submit" class="btn btn-primary btn-block">Ingresar</button>
+            </div>
+            <!-- /.col -->
+          </div>
+        </form>
+
+
+      </div>
+      <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+  </div>
+  <!-- /.login-box -->
+
+  <!-- jQuery -->
+  <script src="../public/template/AdminLTE-3.2.0/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="../public/template/AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../public/template/AdminLTE-3.2.0/dist/js/adminlte.min.js"></script>
+</body>
+
+</html>
